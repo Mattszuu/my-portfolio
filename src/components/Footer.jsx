19409a0f54494cf9e5048eb5a6d150eaa40f333a
Mattszuu <Mattuntalan2@gmail.com>
2025-09-02 17:25:40 +0800

    Updates

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer({ year }) {
-  return (
-    <footer className="p-4 text-center border-t border-slate-300 dark:border-slate-700">
-      <p>© {year} Christian Matthew Untalan. All rights reserved.</p>
-    </footer>
-  );
-}
